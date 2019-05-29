b7229587d6ee9b047b1df1aac07a9f7fa041dc5a
Steffen <steffen@opencampus.sh>
2019-05-29 15:32:09 +0100

    Coffee Break Version 2

--- PISA_analysis.R
+++ PISA_analysis.R
@@ -19,6 +19,9 @@ load("PISA_2015_GERMANY.Rda")
 # Funtion to retrieve all variable names in the data table
 names(PISA_2015_GERMANY)
 
+PISA_2015_GERMANY <- PISA_2015_GERMANY %>%
+  rename(gender=ST004D01T)
+
 # Contruct data table with only the items from the reading assessment
 resp <- PISA_2015_GERMANY[c("CR055Q01S","DR055Q02C","DR055Q03C","DR055Q05C","CR104Q01S","CR104Q02S", 
                             "CR104Q05S","CR111Q01S","DR111Q02BC","DR111Q06C","CR227Q01S","CR227Q02S",
@@ -56,10 +59,8 @@ tam.ctt(resp, wlescore=wle$theta)
 plot(mod)
 
 # Gender DIF
-
-formulaA <- ~item+gender+item*gender
-facets <- as.data.frame(gender)
-mod2 <- tam.mml.mfr( resp= scored, facets= facets , formulaA = formulaA )
+mod_dif_d <- tam.mml.mfr(resp=resp_d, facets=PISA_2015_GERMANY, formulaA=~item+gender+item*gender)
+summary(mod_dif_d)
 
 
 
